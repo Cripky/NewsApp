635b326191cc44496c80c3275fa88ada699a679a
pesherov <maxpesherov@gmail.com>
2024-01-15 14:59:38 +0500

    Add onboarding screen

--- app/src/main/java/com/example/newsapp/presentation/onboarding/Page.kt
+++ app/src/main/java/com/example/newsapp/presentation/onboarding/Page.kt
@@ -12,18 +12,18 @@ data class Page(
 
 val pages = listOf(
     Page(
-        title = "Lorem Ipsum is simply dummy",
-        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        title = "TOP 20 PLAYERS OF 2023: ZYWOO (1)",
+        description = "ZywOo was unmatched in the race to become HLTV’s Player of the Year, leading the stat boards in over a dozen categories.",
         image = R.drawable.onboarding1
     ),
     Page(
-        title = "Lorem Ipsum is simply dummy",
-        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        title = "TOP 20 PLAYERS OF 2023: NIKO (2)",
+        description = "NiKo's exceptional numbers at the biggest events and against the best teams see him tie his best placement on our Top 20 Players of the Year ranking.",
         image = R.drawable.onboarding2
     ),
     Page(
-        title = "Lorem Ipsum is simply dummy",
-        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        title = "TOP 20 PLAYERS OF 2023: ROPZ (3)",
+        description = "Impressive stats in the biggest tournaments and against elite-level opposition earn the Estonian the third place in HLTV’s Top 20 Players of 2023 ranking.",
         image = R.drawable.onboarding3
     )
 )
