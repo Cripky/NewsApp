17f76d2ca161069e3d8fce1eeae54aa9da183fb7
pesherov <maxpesherov@gmail.com>
2024-02-19 17:52:22 +0500

    Start NewsNavigator

---
+++ app/src/main/java/com/example/newsapp/presentation/news_navigator/NewsNavigator.kt
@@ -0,0 +1,94 @@
+package com.example.newsapp.presentation.news_navigator
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.newsapp.R
+import com.example.newsapp.presentation.navgraph.Route
+import com.example.newsapp.presentation.news_navigator.components.BottomNavigationItem
+import com.example.newsapp.presentation.news_navigator.components.NewsBottomNavigation
+
+@Composable
+fun NewsNavigator() {
+
+    val bottomNavigationItems = remember {
+        listOf(
+            BottomNavigationItem(icon = R.drawable.ic_home, text = "Home"),
+            BottomNavigationItem(icon = R.drawable.ic_search, text = "Search"),
+            BottomNavigationItem(icon = R.drawable.ic_bookmark, text = "Bookmark"),
+        )
+    }
+
+    val navController = rememberNavController()
+    val backstackState = navController.currentBackStackEntryAsState().value
+    var selectedItem by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    selectedItem = when (backstackState?.destination?.route) {
+        Route.HomeScreen.route -> 0
+        Route.SearchScreen.route -> 1
+        Route.BookmarkScreen.route -> 2
+        else -> 0
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NewsBottomNavigation(
+                items = bottomNavigationItems,
+                selectedItem = selectedItem,
+                onItemClick = { index ->
+                    when(index) {
+                        0 -> navigateToTab(
+                            navController = navController,
+                            route = Route.HomeScreen.route
+                        )
+
+                        1 -> navigateToTab(
+                            navController = navController,
+                            route = Route.SearchScreen.route
+                        )
+
+                        2 -> navigateToTab(
+                            navController = navController,
+                            route = Route.BookmarkScreen.route
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        val bottomPadding = it.calculateBottomPadding()
+        NavHost(
+            navController = navController,
+            startDestination = Route.HomeScreen.route,
+            modifier = Modifier.padding(bottom = bottomPadding)
+        ) {
+
+        }
+    }
+}
+
+fun navigateToTab(navController: NavController, route: String) {
+    navController.navigate(route) {
+        navController.graph.startDestinationRoute?.let { homeScreen ->
+            popUpTo(homeScreen) {
+                saveState = true
+            }
+            restoreState = true
+            launchSingleTop = true
+        }
+    }
+}
